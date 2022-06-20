715eb7fa15ca792969f5fafafd7cde38437e2783
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-06-20 11:13:23 -0700

    Add functions to check for functions and tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,59 @@
+use std::fmt;
 use std::fs;
 
 struct Source {
-    functions: Vec::<String>,
-    tests: Vec::<String>,
+    functions: Vec<String>,
+    tests: Vec<String>,
 }
 
-fn read_tests_and_functions(path: &str) {
-    let source = Source {
+impl fmt::Display for Source {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut out = String::from("Source:\n");
+        out += "  Functions:\n";
+        for func in &self.functions {
+            out += &format!("    {func},\n").to_string();
+        }
+        out += "  Tests:\n";
+        for test in &self.tests {
+            out += &format!("    {test},\n").to_string();
+        }
+
+        write!(f, "{}", out)
+    }
+}
+
+fn isolate_functions_and_tests(contents: &str, source_ref: &mut Source) {
+    let mut prev_line: &str = "";
+    for mut line in contents.split("\n") {
+        // The next line of code checks for fn, but also contains fn
+        // But it also contains "exactly this" so it won't be caught
+        // by the search for functions
+        if line.contains("fn ") && line.contains("(") && !line.contains("exactly this") {
+            line = line.trim_start();
+            let parts: Vec<&str> = line.split("(").collect();
+            line = parts[0];
+
+            if prev_line.contains("#[test]") {
+                source_ref.tests.push(line.to_string());
+            } else {
+                source_ref.functions.push(line.to_string());
+            }
+        }
+        prev_line = line;
+    }
+}
+
+fn read_tests_and_functions(path: &str, source_ref: &mut Source) {
+    let contents = fs::read_to_string(path).expect("Could not open file");
+    isolate_functions_and_tests(&contents, source_ref);
+}
+
+fn walk() -> Source {
+    let mut source = Source {
         functions: Vec::<String>::new(),
         tests: Vec::<String>::new(),
     };
 
-
-}
-
-fn walk() {
     let paths = fs::read_dir("./").unwrap();
 
     for path in paths {
@@ -22,11 +61,28 @@ fn walk() {
         let path_name = new_path.to_string_lossy();
 
         if path_name.contains(".rs") {
-            read_tests_and_functions(&path_name);
+            read_tests_and_functions(&path_name, &mut source);
         }
     }
+
+    return source;
 }
 
 fn main() {
-    walk();
+    let source = walk();
+    println!("{}", source);
+}
+
+fn _function_to_test() -> i64 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn _function_to_test_test() {
+        assert!(_function_to_test() == 0);
+    }
 }
